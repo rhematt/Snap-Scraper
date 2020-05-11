723eaeea4ca7034fc40e1a9dd42f2141312a8208
Dr Richard Matthews <rhematthews@gmail.com>
2020-05-11 11:24:15 +0930

    Initial Commit

---
+++ SC-MapDownloader/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SC-MapDownloader
+//
+//  Created by Richard Matthews on 11/5/20.
+//  Copyright © 2020 Richard Matthews. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
